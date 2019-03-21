38d15d0608f9b04afa0f573dae73e30537e0a12f
Arjit Sharma <arjits@arjits-mbp.home>
2019-03-21 22:33:51 +0000

    Added the Context and Entities and Dummy Data

---
+++ Context/DummyData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using SamplWebApp.Model;
+
+namespace SamplWebApp.Context
+{
+    public class DummyData
+    {
+        public static void Initialize(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<DaysContext>();
+                context.Database.EnsureCreated();
+                if (context.Days != null && context.Days.Any())
+                    return;
+                var Days = GetDays().ToArray();
+                context.Days.AddRange(Days);
+                context.SaveChanges();
+            }
+
+        }
+        public static List<Days>GetDays()
+        {
+            List<Days> days = new List<Days>
+            {
+                new Days{ Id = 1, DayName = "Sunday"},
+                new Days{ Id = 2, DayName = "Monday"},
+                new Days{ Id = 3, DayName = "Tuesday"},
+                new Days{ Id = 4, DayName = "Wednesday"},
+                new Days{ Id = 5, DayName = "Thursday"},
+                new Days{ Id = 6, DayName = "Friday"},
+                new Days{ Id = 7, DayName = "Saturday"},
+
+            };
+
+            return days;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SamplWebApp.Context;
 
 namespace SamplWebApp
 {
@@ -25,6 +27,9 @@ namespace SamplWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
+            services.AddDbContext<DaysContext>(opts => opts.UseMySql(Configuration["ConnectionString:DefaultConnection"]));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -43,6 +48,7 @@ namespace SamplWebApp
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            DummyData.Initialize(app);
         }
     }
 }
